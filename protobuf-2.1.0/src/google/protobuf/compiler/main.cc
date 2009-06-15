90143d9e6530df11bf6516120c61c5a54dd71246
simon.kallweit <simon.kallweit@cc016f86-575a-11de-bbaa-f1a44a2b9d47>
2009-06-15 07:32:48 +0000

    added new lwpb compiler
    git-svn-id: http://lwpb.googlecode.com/svn/trunk@4 cc016f86-575a-11de-bbaa-f1a44a2b9d47

--- protobuf-2.1.0/src/google/protobuf/compiler/main.cc
+++ protobuf-2.1.0/src/google/protobuf/compiler/main.cc
@@ -34,6 +34,7 @@
 #include <google/protobuf/compiler/cpp/cpp_generator.h>
 #include <google/protobuf/compiler/python/python_generator.h>
 #include <google/protobuf/compiler/java/java_generator.h>
+#include <google/protobuf/compiler/lwpb/lwpb_generator.h>
 
 
 int main(int argc, char* argv[]) {
@@ -55,6 +56,11 @@ int main(int argc, char* argv[]) {
   google::protobuf::compiler::python::Generator py_generator;
   cli.RegisterGenerator("--python_out", &py_generator,
                         "Generate Python source file.");
+
+  // Proto2 lwpb
+  google::protobuf::compiler::lwpb::Generator lwpb_generator;
+  cli.RegisterGenerator("--lwpb_out", &lwpb_generator,
+                        "Generate lwpb source file.");
 
   return cli.Run(argc, argv);
 }
