f1819786e677118172fe8841ba6b749c534c5848
Alan Grow <alangrow@gmail.com>
2013-01-04 15:03:38 -0700

    expose enumeration symbol values under codec.enums. this closes GH-9

--- python/lwpb/codec.py
+++ python/lwpb/codec.py
@@ -3,7 +3,7 @@ import lwpb
 
 class MessageCodec:
 
-  def __init__(self, typename="", pb2=None, pb2file=None):
+  def __init__(self, typename="", pb2=None, pb2file=None, filenum=0):
 
     if pb2file != "":
       pb2 = file(pb2file).read()
@@ -17,9 +17,18 @@ class MessageCodec:
     pb2_type_fds = pb2_types['google.protobuf.FileDescriptorSet']
 
     self.definition = self.decoder.decode(pb2, pb2_descriptor, pb2_type_fds)
-    self.descriptor = lwpb.Descriptor(self.definition['file'][0])
+    self.descriptor = lwpb.Descriptor(self.definition['file'][filenum])
     self.types = self.descriptor.message_types()
     self.messagetype = self.types[typename]
+
+    # Expose enumeration values as self.enums.<Name>.<Member>
+
+    self.enums = ProtoEnums()
+    for e in self.definition['file'][filenum].get('enum_type',[]):
+      enum = ProtoEnum()
+      for v in e['value']:
+        setattr(enum,v['name'],v['number'])
+      setattr(self.enums,e['name'],enum)
 
 
   def encode(self, record):
@@ -27,4 +36,11 @@ class MessageCodec:
 
   def decode(self, data):
     return self.decoder.decode(data, self.descriptor, self.messagetype)
+
+
+# These are used to expose enumeration symbols and values.
+
+class ProtoEnums(object): pass
+class ProtoEnum(object): pass
+
 
